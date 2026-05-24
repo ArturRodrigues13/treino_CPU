270a10762f14101e8947c853845344d756ec5333
Artur Rodrigues Santos <arturrodriguesfla13@gmail.com>
2026-05-24 00:24:49 -0300

    melhora Boi + script do bem

--- CPU_2025/Boi/boi.py
+++ CPU_2025/Boi/boi.py
@@ -7,44 +7,19 @@ alegobois = [int(item) for item in input().split(" ")]
 pesos = [int(item) for item in input().split(" ")]
 
 bois = []
-notas = [0] * alegobois[1]
-
-maior = 0
-i_maior = 0
-
-apresentacoes = []
 
 for x in range(alegobois[1]):
 
-	b, *apresentacao = input().split(" ")
-	bois.append(b)
-	apresentacao = [float(item) for item in apresentacao]
-	apresentacoes.append(apresentacao)
+	boi = []
+	nome, *notas = input().split(" ")
+	boi.append(nome)
+	notas = [float(item) for item in notas]
+	boi.append(notas)
+	boi.append(sum(x * p for (x, p) in zip(notas, pesos)))
+	bois.append(boi)
 
-	for y in range(alegobois[0]):
-		notas[x] += apresentacao[y] * pesos[y]
+bois.sort(key=lambda x:(x[2],*[i for i in x[1]]),reverse=True)
 
-while True:
-
-	if(len(bois) == 0):
-		break
-
-	maior = max(notas)
-	i_maior = notas.index(maior)
-
-	if(notas.count(maior) > 1):
-		indices = [i for i, valor in enumerate(notas) if valor == maior]
-		empatados = [[bois[i], apresentacoes[i]] for i in indices]
-		empatados.sort(key=lambda x:(x[1]),reverse=True)
-		print(*[e[0] for e in empatados],sep="\n")
-		bois = [b for (i, b) in enumerate(bois) if i not in indices]
-		notas = [n for (i, n) in enumerate(notas) if i not in indices]
-		apresentacoes = [a for (i, a) in enumerate(apresentacoes) if i not in indices]
-
-	else:
-		print(bois[i_maior])
-		bois.pop(i_maior)
-		apresentacoes.pop(i_maior)
-		notas.pop(i_maior)
+print(*[nome[0] for nome in bois],sep="\n")
 
 fim = time.perf_counter()
--- CPU_2025/Boi/teste.sh
+++
@@ -1,4 +0,0 @@
-for i in {1..10}; do
-	echo visitando binga $i
-	python boi.py < input/boi$i | diff - output/boi$i --strip-trailing-cr
-done
